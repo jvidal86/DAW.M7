a339859bea741b835b99ea217f584e5a2900e95f
jvidal86 <44492197+jvidal86@users.noreply.github.com>
2020-10-22 17:39:15 +0200

    Create formulari_un_arxiu.php
    formulari logging en un arxiu

---
+++ UF1/T2_AppDinamiquesPHP/formulari_un_arxiu.php
@@ -0,0 +1,41 @@
+
+
+
+
+<?php
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        if($_POST['usuari'] == 'admin' and $_POST['clau'] == '1234'){
+            header("Location: principal.php");
+        } else {
+            $err = true;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulari Bàsic</title>
+</head>
+<body>
+    <?php if(isset($err)){
+        echo '<p> Revisa usuari i clau</p>';
+    }
+
+    ?>
+    <form method="POST"
+          action= ?>" >
+
+        <label for="usuari">Usuari</label>
+        <input name="usuari" type="text" id="usuari"
+                value="<?php if(isset($usuari)) echo $usuari; ?>"/> <br/>
+
+        <label for = "clau">Clau</label>
+        <input id ="clau" name="clau" type="password"/> <br/>
+
+        <input type="submit" value="Enviar"/><br/>
+    </form>
+
+</body>
+</html>
